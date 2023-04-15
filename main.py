c782d830fadf0d64e02ee25e6597ce4e1f6596fe
Kevan Jordan <kevanjordan9@gmail.com>
2023-04-15 17:48:19 +0100

    finshed imagePilxation logic

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 
-#open image
-img=Image.open('photo of me.PNG')
+def pixleateImage():
+    #open image
+    img=Image.open('photo of me.PNG')
 
-plt.imshow(img)
-plt.show()
+    plt.imshow(img)
+    plt.show()
+
+    small_img=img.resize((8,8),Image.BILINEAR)
+    plt.imshow(small_img)
+    plt.show()
+
+pixleateImage()
