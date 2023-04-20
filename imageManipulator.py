5b372e829051fe5387d995df423f2be71feed9a5
Kevan Jordan <kevanjordan9@gmail.com>
2023-04-20 20:28:06 +0100

    added more images to test and added cartooon drawing

--- imageManipulator.py
+++ imageManipulator.py
@@ -3,6 +3,21 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def color_quantization(img, k):
+# Transform the image
+  data = np.float32(img).reshape((-1, 3))
+
+# Determine criteria
+  criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+
+# Implementing K-Means
+  ret, label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+  center = np.uint8(center)
+  result = center[label.flatten()]
+  result = result.reshape(img.shape)
+  return result
+
 def pixleateImage(img):
     height, width = img.shape[:2]
     w,h=(32,32)
@@ -17,12 +32,23 @@ def imageToSketch(img):
     sketch_img = cv2.divide(img_gray, 255 - img_smoothing, scale=255)
     return sketch_img
 
+def comicImage(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.medianBlur(img_gray, 7)
+    edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+    total_color = 9
+    img = color_quantization(img, total_color)
+    blurred = cv2.bilateralFilter(img, d=7, sigmaColor=200,sigmaSpace=200)
+    cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+    return cartoon
+
 
 def displayImages():
-    img=cv2.imread('photo of me.PNG')
+    img=cv2.imread('images.jpg')
     img= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     pixel_img  = pixleateImage(img)
     sketch_img = imageToSketch(img)
+    cartoon_img = comicImage(img)
 
     plt.figure(figsize=(20,20))
     plt.subplot(1,5,1)
@@ -36,6 +62,11 @@ def displayImages():
     plt.axis("off")
 
     plt.subplot(1,5,3)
+    plt.title("cartoon Image")
+    plt.imshow(cartoon_img)
+    plt.axis("off")
+
+    plt.subplot(1,5,4)
     plt.title("sketch Image")
     plt.imshow(sketch_img,cmap="gray",vmin=0,vmax=255)
     plt.axis("off")
